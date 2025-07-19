92865807c32c3a8480738d3a99ad75f98d21bc8a
vadiraj_22 <vadirajjoshi22504@gmail.com>
2025-07-19 23:52:21 +0530

    frontend error solved

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
 import AiTools from '../components/AiTools'
 import Testimonials from '../components/Testimonials'
-import Plan from '../components/plan'
+import Plan from '../components/Plan'
 import Footer from '../components/Footer'
 
 const Home = () => {
