7bc8270b776a8e04e07d67ce7e1a95d1ee2cad42
vadiraj-22 <vadirajjoshi22504@gmail.com>
2025-10-07 10:57:34 +0530

    error fix in generate images

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -9,7 +9,8 @@ export const auth  = async (req , res , next)=>{
 
         const user = await clerkClient.users.getUser(userId)
 
-        if( hasPremiumPlan && user.privateMetadata.free_usage){
+        // Check if free_usage exists in metadata, if not initialize it to 0
+        if(user.privateMetadata.free_usage !== undefined){
             req.free_usage = user.privateMetadata.free_usage
         }
         else{
