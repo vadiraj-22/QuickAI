5045000dd27f4261befbb94f5661c559643d5a46
vadiraj-22 <vadirajjoshi22504@gmail.com>
2025-12-30 00:24:52 +0530

    gemini API key error fix

--- server/controllers/aiController.js
+++ server/controllers/aiController.js
@@ -27,7 +27,7 @@ export const generateArticle = async (req, res) => {
         }
 
         const response = await AI.chat.completions.create({
-            model: "gemini-2.0-flash",
+            model: "gemini-1.5-flash",
             messages: [{
                 role: "user",
                 content: prompt,
