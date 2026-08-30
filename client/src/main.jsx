652eb24fce284558b6d58f685ab267d4e104da1a
vadiraj-22 <vadirajjoshi22504@gmail.com>
2026-08-30 20:17:54 +0530

    Guarantee Clerk development publishable key fallback in production build

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,7 +5,7 @@ import {BrowserRouter} from 'react-router-dom'
 import { ClerkProvider } from '@clerk/clerk-react'
 
 // Import your Publishable Key
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY || 'pk_test_YnJpZ2h0LWJhc2lsaXNrLTc2LmNsZXJrLmFjY291bnRzLmRldiQ'
 
 if (!PUBLISHABLE_KEY) {
   throw new Error('Missing Publishable Key')
